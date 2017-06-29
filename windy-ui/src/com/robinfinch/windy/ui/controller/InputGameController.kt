af7759f7d88bd03f4c63ccdb3d884250dc26d3dd
Mark Hoogenboom <mark@robinfinch.com>
2017-06-29 13:45:16 +0100

    Input game controller

---
+++ windy-ui/src/com/robinfinch/windy/ui/controller/InputGameController.kt
@@ -0,0 +1,88 @@
+package com.robinfinch.windy.ui.controller
+
+import com.robinfinch.windy.core.game.Action
+import com.robinfinch.windy.core.game.Arbiter
+import com.robinfinch.windy.core.game.Game
+import com.robinfinch.windy.core.text.format
+import com.robinfinch.windy.ui.getString
+import java.io.BufferedWriter
+import java.io.StringWriter
+import java.util.*
+
+class InputGameController(private val view: View, private val texts: ResourceBundle) {
+
+    private val arbiter: Arbiter = Arbiter()
+
+    private var whiteHasTheBoard: Boolean = true
+
+    fun onStart() {
+        arbiter.setupGame()
+
+        view.setTitle(texts.getString("app.setting_up"))
+        view.setBoard(arbiter.currentPosition)
+        view.setHistory("")
+
+        view.enterGameDetails(this::onGameDetailsEntered)
+    }
+
+    fun onGameDetailsEntered(white: String, black: String) {
+        arbiter.white = white
+        arbiter.black = black
+
+        view.setTitle("Entering ${white} - ${black}")
+        view.enableMovesOnBoard(this::onActionEntered)
+        view.enableAcceptDraw(this::onActionEntered)
+        view.enableResign(this::onActionEntered)
+
+        whiteHasTheBoard = true
+        play()
+    }
+
+    private fun onActionEntered(action: Action): Boolean {
+
+        if (whiteHasTheBoard) {
+            if (arbiter.acceptWhite(action)) {
+                whiteHasTheBoard = false
+                play()
+                return true
+            } else {
+                return false
+            }
+        } else {
+            if (arbiter.acceptBlack(action)) {
+                whiteHasTheBoard = true
+                play()
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    private fun play() {
+
+        if (arbiter.result == Game.Result.UNKNOWN) {
+            val history = StringWriter()
+            BufferedWriter(history).use { out -> arbiter.history.format(out, html = true) }
+
+            view.setBoard(arbiter.currentPosition)
+            view.setHistory(history.toString())
+        } else {
+            saveGame()
+        }
+    }
+
+    private fun saveGame() {
+
+        view.enableMovesOnBoard(null)
+        view.enableAcceptDraw(null)
+        view.enableResign(null)
+
+        val file = view.showSaveDialog()
+        if (file != null) {
+            arbiter.writeGame(file)
+        }
+
+        onStart()
+    }
+}
